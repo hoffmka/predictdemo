caf6fc818f41c007c032454ed6751ed5b96028b9
Katja Hoffmann <katja.hoffmann@tu-dresden.de>
2020-05-10 22:15:39 +0200

    BugFix: Deleting RTest.R url

--- apps/predictions/urls.py
+++ apps/predictions/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('list', views.prediction_list, name='prediction_list'),
     path('create', views.create_magpie_prediction, name='prediction_create'),
     path('magpie_download/<int:prediction_id>/', views.magpie_download_jobresults, name='magpie_download_jobresults'),
-    path('Rtest', views.test_R, name='test_R'), # only for testing R integration
 ]
