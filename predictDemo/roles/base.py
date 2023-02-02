7416342482816f49bd227c4257c0da19bd8bdc40
hoffmka <katja.hoffmann@tu-dresden.de>
2023-02-02 20:36:56 +0100

    Delete not needed roles

--- predictDemo/roles/base.py
+++ predictDemo/roles/base.py
@@ -4,8 +4,3 @@
 
 # roles from trials apps
 from predictDemo.roles.ttp_domains import *
-
-class admin(AbstractUserRole):
-     available_permissions = {
-        #'dept_haematology:can_access_data': TRUE,
-     }
